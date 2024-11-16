b4143622e14ce02dc543894e97887deadceb0c30
Luca Avveduto <avvedutoluca@itis-molinari.eu>
2024-11-16 19:42:18 +0100

    loading to fix

--- src/index.js
+++ src/index.js
@@ -18,6 +18,16 @@ String.prototype.deleteSpace = function () {
     return this.replaceAll(/\s/g, "");
 }
 
+function hide (element) {
+    element.classList.remove("show");
+    element.classList.add("hidden");
+}
+
+function show (element) {
+    element.classList.remove("hidden");
+    element.classList.add("show");
+}
+
 const formElement = document.getElementById("form");
 const navbarElement = document.getElementById("nav-bar");
 const modalElement = document.getElementById("md");
@@ -52,6 +62,7 @@ navbar.build("Monitora gli incidenti", "Inserisci");
 navbar.render();
 
 f.onsubmit((result) => {
+    show(loading);
     console.log(result);
     if (!result) return false;
     if (result.lenght < 5) return false;
@@ -82,32 +93,19 @@ f.onsubmit((result) => {
                     let data = JSON.parse(d);
                     if (!data[key]) {
                         data[key] = res;
-                        fetchCache.setData(data).then(console.log).catch(console.error);
+                        fetchCache.setData(data).then(hide(loading)).catch(console.error);
                         map.addPlace((result[0] + ", Milano"), address[0]).then((i) => {
                             map.render(i);
-                            loading.classList.add("hidden");
-                             //indirizzo da inserire
                         }).catch((i) => {
                             map.render(i);
-                            loading.classList.add("hidden");
                         });
                         modalElement.classList.remove("show");
                         modalElement.classList.add("hidden");
                     } else return false; //Già esistente
                 }).catch(console.error);
             }).catch(console.error);
-
         }).catch(console.error);
     }).catch(console.error);
-    loading.classList.add("show");
-    loading.innerHTML =
-    `<div class="flex-col gap-4 w-full flex items-center justify-center">
-  <div class="w-28 h-28 border-8 text-blue-400 text-4xl animate-spin border-gray-300 flex items-center justify-center border-t-blue-400 rounded-full">
-    <svg viewBox="0 0 24 24" fill="currentColor" height="1em" width="1em" class="animate-ping">
-      <path d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z"></path>
-    </svg>
-  </div>
-</div>`;
 });
 table.searchCallback((inputElement)=>{
     const inputValue = inputElement.value;
