1de688b2e9c43031939236e4cc642d1926d2fd96
AngeloRipamonti <ripamontiangelo@itis-molinari.eu>
2024-11-16 17:47:16 +0100

    save marker on map

--- src/scripts/mappe.js
+++ src/scripts/mappe.js
@@ -1,3 +1,5 @@
+import { generateFetchComponent } from "./fetchCache.js";
+
 export function createMap(parentElement) {
     const zoom = 12;
     const maxZoom = 19;
@@ -6,12 +8,26 @@ export function createMap(parentElement) {
 
     return {
         build: () => {
-            if(map) map.remove();
-            map = L.map(parentElement).setView([45.4639102, 9.1906426], zoom);
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: maxZoom,
-                attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(map);
+            return new Promise((resolve, reject) => {
+                const fetchCache = generateFetchComponent();
+                fetchCache.build("../../config.json","cache").then(()=>{
+                    fetchCache.getPostData().then((d)=>{
+                        let data = JSON.parse(d);
+                        if(map) map.remove();
+                        map = L.map(parentElement).setView([45.4639102, 9.1906426], zoom);
+                        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                            maxZoom: maxZoom,
+                            attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                        }).addTo(map);
+                        for(const key in data){
+                            places.push({name: data[key].address.display_name, coords: [data[key].address.lat, data[key].address.lon]});
+                            const marker = L.marker([data[key].address.lat, data[key].address.lon]).addTo(map);
+                            marker.bindPopup("<b>" + data[key].address.display_name + "</b>");
+                        }
+                        resolve("build map done");
+                    }).catch(reject);
+                }).catch(reject);
+            })
         },
         render: (index) => {
             if(!index) index = 0;
